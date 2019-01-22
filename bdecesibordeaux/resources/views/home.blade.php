fa644780e2628aec30fdb34e15b1cb10954989b3
ilnarra <33094412+ilnarra@users.noreply.github.com>
2019-01-22 17:03:09 +0100

    logout

--- bdecesibordeaux/resources/views/home.blade.php
+++ bdecesibordeaux/resources/views/home.blade.php
@@ -16,6 +16,21 @@
 
                     You are logged in!
                 </div>
+                <li class="nav-item dropdown">
+                              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                  {{ Auth::user()->name }} <span class="caret"></span>
+                              </a>
+      
+                              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                  <a class="dropdown-item" href="{{ route('logout') }}"
+                                     onclick="event.preventDefault();
+                                                   document.getElementById('logout-form').submit();">
+                                      {{ __('Logout') }}
+                                  </a>
+      
+                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                      @csrf
+                                  </form>
             </div>
         </div>
     </div>
