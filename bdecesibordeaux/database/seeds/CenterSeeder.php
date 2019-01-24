ebe54b89f9acd04d084a566c114234bbe30da53f
PtitCaillou <33094458+PtitCaillou@users.noreply.github.com>
2019-01-24 08:42:46 +0100

    fixed seed

--- bdecesibordeaux/database/seeds/CenterSeeder.php
+++ bdecesibordeaux/database/seeds/CenterSeeder.php
@@ -11,15 +11,13 @@ class CenterSeeder extends Seeder
      */
     public function run()
     {
-        DB::connection('mysql2')->insert('insert into center (center) values (?)', ['Arras']);
-        /*
-            , ['Caen'], ['Lille'], ['Rouen'],
+        DB::connection('mysql2')->insert('insert into center (center) values (?)', ['Arras'], ['Caen'], ['Lille'], ['Rouen'],
                                                                                  ['Angouleme'], ['Brest'], ['La Rochelle'], ['Le Mans'], ['Nantes'], ['Saint-Nazaire'],
                                                                                  ['Paris'], ['Orleans'],
                                                                                  ['Dijon'], ['Nancy'], ['Reims'], ['Strasbourg'],
                                                                                  ['Bordeaux'], ['Montpellier'], ['Pau'], ['Toulouse'],
                                                                                  ['Aix-en-Provence'], ['Grenoble'], ['Lyon'], ['Nice'],
                                                                                  ['Alger'], ['Iberia'], ['Cameroun']);
-                                                                                 */
+                                                                                 
     }
 }
