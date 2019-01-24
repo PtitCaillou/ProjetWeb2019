8281a169c574b596bd98746c30323ee3e316648c
ilnarra <33094412+ilnarra@users.noreply.github.com>
2019-01-24 09:49:04 +0100

    seeder connection

--- bdecesibordeaux/database/seeds/UserNationalSeeder.php
+++ bdecesibordeaux/database/seeds/UserNationalSeeder.php
@@ -11,8 +11,8 @@ class UserNationalSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('User')->insert(['lastname' => 'Jean', 'name' => 'Jean', 'email' => 'Jean.jean@viacesi.fr', 'password' => 'jeanJean12', 'center_id' => '17']);
-        DB::table('User')->insert(['lastname' => 'Jeanne', 'name' => 'Jeanne', 'email' => 'Jeanne.jeanne@viacesi.fr', 'password' => 'jeanneJeanne13', 'center_id' => '17']);
-        DB::table('User')->insert(['lastname' => 'Lamouche', 'name' => 'zebi', 'email' => 'zebi.lamouche@viacesi.fr', 'password' => 'zebiLamouche1', 'center_id' => '17']);
+        DB::connection('mysql2')->table('User')->insert(['lastname' => 'Jean', 'name' => 'Jean', 'email' => 'Jean.jean@viacesi.fr', 'password' => 'jeanJean12', 'center_id' => '17']);
+        DB::connection('mysql2')->table('User')->insert(['lastname' => 'Jeanne', 'name' => 'Jeanne', 'email' => 'Jeanne.jeanne@viacesi.fr', 'password' => 'jeanneJeanne13', 'center_id' => '17']);
+        DB::connection('mysql2')->table('User')->insert(['lastname' => 'Lamouche', 'name' => 'zebi', 'email' => 'zebi.lamouche@viacesi.fr', 'password' => 'zebiLamouche1', 'center_id' => '17']);
     }
 }
