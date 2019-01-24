28b1b6c9c2d1a432b2e002cddf096b4818ff110f
ilnarra <33094412+ilnarra@users.noreply.github.com>
2019-01-24 09:41:00 +0100

    dfghjklm

--- bdecesibordeaux/database/seeds/UserSeeder.php
+++ bdecesibordeaux/database/seeds/UserSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Role;
 
 class UserSeeder extends Seeder
 {
@@ -12,9 +11,23 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-      DB::connection('mysql')->insert('insert into User (lastname, name, email, password, role_id) values (?, ?, ?, ?, ?)',
-                                                        ['Jean', 'Jean', 'Jean.jean@viacesi.fr', 'jeanJean12', '2'],
-                                                        ['Jeanne', 'Jeanne', 'Jeanne.jeanne@viacesi.fr', 'jeanneJeanne13', '1'],
-                                                        ['Lamouche', 'zebi', 'zebi.lamouche@viacesi.fr', 'zebiLamouche1', '3']);
+        DB::connection('mysql')->insert('insert into User (lastname, name, email, password, UserStatus_ID) values (?, ?, ?, ?, ?)',
+                                                          ['Jean', 'Jean', 'Jean.jean@viacesi.fr', 'jeanJean12', '2'],
+                                                          ['Jeanne', 'Jeanne', 'Jeanne.jeanne@viacesi.fr', 'jeanneJeanne13', '1'],
+                                                          ['Lamouche', 'zebi', 'zebi.lamouche@viacesi.fr', 'zebiLamouche1', '3']);
+    $role_BDE = Role::where('name', 'BDE')->first();
+    $role_Student  = Role::where('name', 'student')->first();
+    $BDE = new User();
+    $BDE->name = 'BDE Name';
+    $BDE->email = 'BDE@example.com';
+    $BDE->password = bcrypt('secret');
+    $BDE->save();
+    $BDE->roles()->attach($role_BDE);
+    $student = new User();
+    $student->name = 'student Name';
+    $student->email = 'student@example.com';
+    $student->password = bcrypt('secret');
+    $student->save();
+    $student->roles()->attach($role_Student);
     }
 }
