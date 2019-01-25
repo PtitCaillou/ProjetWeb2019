405ed0d09218b9ea268529e8eb770dd1fd303751
ChazeauLeo <33094910+ChazeauLeo@users.noreply.github.com>
2019-01-25 09:29:18 +0100

    unblock

--- bdecesibordeaux/resources/views/footer.blade.php
+++ bdecesibordeaux/resources/views/footer.blade.php
@@ -1,10 +1,46 @@
-@yield('foot')
-  <div class="contact">
-    <ul class="list-item-group">
-        <li class="list-item text-white">264 boulevard godard, 33000, Bordeaux, France</li>
-        <li class="list-item text-white">communication@bdecesibordeaux.fr</li>
+  @yield('foot')
+<!-- Footer -->
+<footer class="foot">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+
+      <!-- Grid row -->
+      <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-2">
+
+          <!-- Content -->
+          <h5 class="text-uppercase">Contact</h5>
+          <h5>264 boulevard godard, <br>33000, Bordeaux, France.<br>communication@bdecesibordeaux.fr.</h5>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-6 mb-md-0 mb-2">
+
+            <!-- Links -->
+            <h5 class="text-uppercase">Links</h5>
+
+    <div class="socialService">
         <li class="list-item text-white"><a href="https://www.instagram.com/bde_cesi_bdx/?hl=fr"><img class="instagram" src="css/instagram.png"></a></li>
         <li class="list-item text-white"><a href="https://www.facebook.com/bdecesibdx/"><img class="facebook" src="css/fb.png"></a></li>
-    </ul>
-  </div>
-    <a class="juridique" href="#">Mentions légales</a>
+        <li class="list-item text-white"><a href="https://twitter.com/BdeCesiBordeaux"><img class="twitter" src="css/twitter.png"'></a></li>
+       
+</div>
+</div>
+    <div class="col-md-6 mt-md-0 mt-2">
+    <!-- Footer Links -->
+    <!-- Copyright -->
+      <a href="http://bdecesibordeaux/"> bdecesibordeaux.fr</a>
+       <a class="juridique" href="#"><br>Mentions légales</a>
+    <!-- Copyright -->
+</div>
+
+  </footer>
+  <!-- Footer -->
+    
