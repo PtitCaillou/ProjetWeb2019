239c73796a33313c0dd32f58fcd026c65f8a73c4
ChazeauLeo <33094910+ChazeauLeo@users.noreply.github.com>
2019-01-28 18:30:29 +0100

    Unblock

--- bdecesibordeaux/resources/views/shop.blade.php
+++ bdecesibordeaux/resources/views/shop.blade.php
@@ -6,31 +6,8 @@
     <div class= "menu-top">
       <a href="{{('add.product')}}" class="btn btn-lg active" role="button" aria-pressed="true" style="background-color: rgb(238, 193, 94);"> Ajouter un produit</a>
       <a href="{{('basket')}}" class="btn btn-lg active" role="button" aria-pressed="true" style="background-color: rgb(238, 193, 94);"> Panier</a>
-      <form  style="position: relative; margin-top: 1rem; max-width: 20%;" action="{{('searchShop')}}" id='search' class="typehead">
-          @include('searchBar')
-      </form>
-       <form style= "position: relative; margin-left: 120%; margin-top: -1rem; max-width: 20%;" action="{{('filter')}}" >
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <label class="input-group-text" for="inputGroupSelect01">Options</label>
-      </div>  
-      <select class="custom-select" id="inputGroupSelect01">
-        <option selected>Choose...</option>
-        @foreach($prod as $prod)
-          <option name="filterCat" value="">
-            {{$prod->type}}
-          </option>
-        @endforeach
-        <option>
-          Prix par ordre croissant
-        </option>
-        <option>
-          Prix par ordre décroissant
-        </option>
-      </select>
-    </div>
-    <button class="btn btn-sm" style="background-color: rgb(238, 193, 94);">Filtrer</button>
-  </form>
+      @include('searchBar')
+</a>
 </div>
   </header>
   @endsection
