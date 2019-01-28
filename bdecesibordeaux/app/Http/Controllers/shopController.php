bf907d2ba224b885c8b6fe6fae84acb841ab2142
PtitCaillou <33094458+PtitCaillou@users.noreply.github.com>
2019-01-28 01:46:47 +0100

    vhosts

--- bdecesibordeaux/app/Http/Controllers/shopController.php
+++ bdecesibordeaux/app/Http/Controllers/shopController.php
@@ -11,7 +11,7 @@ use GuzzleHttp\Client;
 class shopController extends Controller
 {
     public function shop() {
-        $datas = json_decode(file_get_contents('http://siteweb:3000/products'), true);
+        $datas = json_decode(file_get_contents('http://bdecesibordeaux:3000/products'), true);
         $products = [];
         foreach($datas as $data){
             $product = new Product();
@@ -31,7 +31,7 @@ class shopController extends Controller
 
     public function basket(){
         $user_id = auth()->user()->id;
-        $url = "http://siteweb:3000/baskets/uncomplete/" . $user_id;
+        $url = "http://bdecesibordeaux:3000/baskets/uncomplete/" . $user_id;
         $datas = json_decode(file_get_contents($url), true);
         $products = [];
         foreach($datas as $data){
@@ -47,7 +47,7 @@ class shopController extends Controller
         $client = new Client();
         $user_id = auth()->user()->id;
         $product_id = $request->add;
-        $url = "http://siteweb:3000/baskets/add/" . $user_id;
+        $url = "http://bdecesibordeaux:3000/baskets/add/" . $user_id;
         $body['product'] = $product_id;
         $body['quantity'] = "1";
         $body['status'] = "1";
@@ -57,7 +57,7 @@ class shopController extends Controller
 
     public function search(Request $request){
         $research = $request->search;
-        $uri = "http://siteweb:3000/products/" . $research;
+        $uri = "http://bdecesibordeaux:3000/products/" . $research;
         $datas = json_decode(file_get_contents($uri), true);
         $products = [];
         foreach($datas as $data){
