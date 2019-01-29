1e5ac429d9641fe75221a0b343dc89fe578ed560
ChazeauLeo <33094910+ChazeauLeo@users.noreply.github.com>
2019-01-29 17:47:45 +0100

    javascript

--- bdecesibordeaux/resources/views/add-basket.blade.php
+++ bdecesibordeaux/resources/views/add-basket.blade.php
@@ -12,6 +12,12 @@
     header('Location: login'); 
     exit();
   ?>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+  crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+  crossorigin="anonymous"></script>
   <!--  <p>Vous n'etes pas connecté, vous ne pouvez donc pas acceder a cette page.</p><br>
   <p>Pour vous connecter cliquez <a href="{//{('login')}}"> ici</a></p> -->
 @endif
--- bdecesibordeaux/storage/framework/views/59e9969d6bd3516a742324eb2b679e179a68ae88.php
+++ bdecesibordeaux/storage/framework/views/59e9969d6bd3516a742324eb2b679e179a68ae88.php
@@ -1,26 +1,22 @@
+<html>
+</body>
+<?php $__env->startSection('content'); ?>
 <?php if(checkPermission(['bde'])): ?>
 <head>
 	<title>Ajouter un produit</title>
 </head>
-
-<body>
-	<header>
-		<h1>Ajouter un produit</h1>
-		<?php echo $__env->make('menu', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-	</header>
 	<main>
-		<?php echo $__env->yieldContent('body'); ?>
-		<form style="margin-top: 5rem;" method="POST" action="<?php echo e(('store')); ?>">
+		<form style="margin-top: 5rem;" method="POST" action="<?php echo e(('add-product')); ?>">
 	<div class="form-group">
-    <label for="exampleFormControlInput1" style="color: white; position: relative; left: 20%; ">Nom du produit</label>
+    <label for="exampleFormControlInput1" style="color: black; position: relative; left: 20%;">Nom du produit</label>
     <input type="text" name="name" class="form-control" style="position: relative; left: 20%; max-width: 50%; " id="exampleFormControlInput1" placeholder="Titre">
   </div>
   <div class="form-group">
-    <label for="exampleFormControlInput1" style="color: white; position: relative; left: 20%; ">Prix</label>
+    <label for="exampleFormControlInput1" style="color: black; position: relative; left: 20%;">Prix</label>
     <input type="price" name="price" class="form-control" style="position: relative; left: 20%; max-width: 50%; " id="exampleFormControlInput1" placeholder="Titre">
   </div>
    <div class="form-group">
-    <label for="exampleFormControlTextarea1" style="color: white; position: relative; left: 20%; ">Description</label>
+    <label for="exampleFormControlTextarea1" style="color: black; position: relative; left: 20%;">Description</label>
     <textarea class="form-control" name="description" id="exampleFormControlTextarea1" style="position: relative; left: 20%; max-width: 50%;" rows="3"></textarea>
   </div>
   <select style="position: relative; left: 20%; max-width: 50%;" name="type" class="custom-select">
@@ -30,7 +26,7 @@
   <option value="3">Tee shirt</option>
 </select>
   <div class="form-group">
-    <label for="exampleFormControlFile1">Ajouter une image</label>
+    <label for="exampleFormControlFile1" style="color: black; position: relative; left: 20%;">Ajouter une image</label>
     <input type="file" name="img" class="form-control-file" id="exampleFormControlFile1" style=" position: relative; left: 20%; max-width: 50%;">
   </div>
 	<button type="submit" class="btn btn-primary" style= "position: relative; left: 20%;">Ajouter le produit</button>
@@ -38,6 +34,13 @@
 	</main>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
  <link rel="stylesheet" type="text/css" href="css/style.css">
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+  crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+  crossorigin="anonymous"></script>
+<?php $__env->stopSection(); ?>
 </body>
 <?php elseif(checkPermission(['student','employee'])): ?>
 <p>Vous n'etes pas autorisé a acceder a cette page.</p>
@@ -48,3 +51,5 @@
 	exit();
 ?>
 <?php endif; ?>
+</html>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
